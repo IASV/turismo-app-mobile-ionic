23d331eaf94f9147849faba0e0ca52e4a8aeb36b
Iván Suarez <ivan.suarez@ultrabox.com>
2023-09-22 18:06:53 -0500

    added google maps

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,4 +1,4 @@
-import { Redirect, Route } from "react-router-dom";
+import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
   IonContent,
@@ -9,8 +9,8 @@ import {
   IonTabButton,
   IonTabs,
   setupIonicReact,
-} from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
 import {
   businessSharp,
   earth,
@@ -23,41 +23,38 @@ import {
   restaurantSharp,
   square,
   triangle,
-} from "ionicons/icons";
-import Tab1 from "@/pages/Tabs/Tab1";
-import Tab2 from "@/pages/Tabs/Tab2";
-import Tab3 from "@/pages/Tabs/Tab3";
-import Tab4 from "@/pages/Tabs/Tab4";
-
+} from 'ionicons/icons';
+import Tab1 from '@/pages/Tabs/Tab1';
+import Tab2 from '@/pages/Tabs/Tab2';
+import Tab3 from '@/pages/Tabs/Tab3';
+import Tab4 from '@/pages/Tabs/Tab4';
 
 // Alcaldia
-import SecretariaGobierno from "@/pages/Menu/Menu-Alcaldida-SG";
-import SecretariaEducacion from "@/pages/Menu/Menu-Alcladia-SE";
-import SecretariaCulturayDeporte from "@/pages/Menu/Menu-Alcladia-CyD";
+import SecretariaGobierno from '@/pages/Menu/Menu-Alcaldida-SG';
+import SecretariaEducacion from '@/pages/Menu/Menu-Alcladia-SE';
+import SecretariaCulturayDeporte from '@/pages/Menu/Menu-Alcladia-CyD';
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import "../theme/variables.css";
-import Main from "@/pages/Main";
-import CamaraComercio from "@/pages/Camaras/CamaraComercio";
-import Bomberos from "@/pages/Bomberos/Bomberos";
-import Policia from "@/pages/Policia/Policia";
-
-
+import '../theme/variables.css';
+import Main from '@/pages/Main';
+import CamaraComercio from '@/pages/Camaras/CamaraComercio';
+import Bomberos from '@/pages/Bomberos/Bomberos';
+import Policia from '@/pages/Policia/Policia';
 
 function Tabs() {
   return (
@@ -65,77 +62,69 @@ function Tabs() {
       <IonReactRouter>
         <IonTabs>
           <IonRouterOutlet>
+            {/* Main */}
+            <Route exact path='/'>
+              <Main />
+            </Route>
 
-          {/* Main */}
-          <Route exact path="/">             
-              <Main/>
-          </Route>
-
-            <Route exact path="/tab1">
+            <Route exact path='/tab1'>
               <Tab1 />
             </Route>
-            <Route exact path="/tab2">
+            <Route exact path='/tab2'>
               <Tab2 />
             </Route>
-            <Route path="/tab3">
+            <Route path='/tab3'>
               <Tab3 />
             </Route>
-            <Route path="/tab4">
+            <Route path='/tab4'>
               <Tab4 />
             </Route>
 
-
-            <Route path="/menu-alcaldia-sg">
-            <SecretariaGobierno/>
+            <Route path='/menu-alcaldia-sg'>
+              <SecretariaGobierno />
             </Route>
 
-            <Route path="/menu-alcaldia-se">
-            <SecretariaEducacion/>
+            <Route path='/menu-alcaldia-se'>
+              <SecretariaEducacion />
             </Route>
 
-            <Route path="/menu-alcaldia-cyd">
-            <SecretariaCulturayDeporte/>
+            <Route path='/menu-alcaldia-cyd'>
+              <SecretariaCulturayDeporte />
             </Route>
 
-            <Route path="/menu-camara-comercio">
-            <CamaraComercio/>
+            <Route path='/menu-camara-comercio'>
+              <CamaraComercio />
             </Route>
 
-            <Route path="/menu-bomberos">
-            <Bomberos/>
+            <Route path='/menu-bomberos'>
+              <Bomberos />
             </Route>
 
-            <Route path="/menu-policia">
-            <Policia/>
+            <Route path='/menu-policia'>
+              <Policia />
             </Route>
-
-
-
           </IonRouterOutlet>
 
-
-          <IonTabBar slot="bottom">
-
-            <IonTabButton tab="tab1" href="/tab1">
-              <IonIcon aria-hidden="true" icon={restaurantSharp} />
+          <IonTabBar slot='bottom'>
+            <IonTabButton tab='tab1' href='/tab1'>
+              <IonIcon aria-hidden='true' icon={restaurantSharp} />
               <IonLabel>Comidas</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="tab2" href="/tab2">
-              <IonIcon aria-hidden="true" icon={earth} />
+            <IonTabButton tab='tab2' href='/tab2'>
+              <IonIcon aria-hidden='true' icon={earth} />
               <IonLabel>Turismo</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="tab3" href="/tab3">
-              <IonIcon aria-hidden="true" icon={businessSharp} />
+            <IonTabButton tab='tab3' href='/tab3'>
+              <IonIcon aria-hidden='true' icon={businessSharp} />
               <IonLabel>Hoteles</IonLabel>
             </IonTabButton>
 
-            <IonTabButton tab="tab4" href="/tab4">
-              <IonIcon aria-hidden="true" icon={medicalSharp} />
+            <IonTabButton tab='tab4' href='/tab4'>
+              <IonIcon aria-hidden='true' icon={medicalSharp} />
               <IonLabel>Drogueria</IonLabel>
             </IonTabButton>
-            
           </IonTabBar>
         </IonTabs>
       </IonReactRouter>
--- src/pages/Tabs/Tab3.tsx
+++ src/pages/Tabs/Tab3.tsx
@@ -1,26 +1,26 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect } from 'react';
 import {
   IonAccordion,
   IonAccordionGroup,
   IonItem,
   IonLabel,
   IonImg,
-} from "@ionic/react";
-import { HotelView } from "./HotelView";
+  IonPage,
+  IonContent,
+} from '@ionic/react';
+import { HotelView } from './HotelView';
 
-import mfachada from "../../assets/hoteles/Morrocoy/hotelmorrocoy.jpg";
-import mrecepcion from "../../assets/hoteles/Morrocoy/hotelmorrocy-recepcion.jpg";
-import mhabitacion from "../../assets/hoteles/Morrocoy/hotelmorrocoy-habitacion.png";
+import mfachada from '../../assets/hoteles/Morrocoy/hotelmorrocoy.jpg';
+import mrecepcion from '../../assets/hoteles/Morrocoy/hotelmorrocy-recepcion.jpg';
+import mhabitacion from '../../assets/hoteles/Morrocoy/hotelmorrocoy-habitacion.png';
 
-import dfachada from "../../assets/hoteles/deLeon/Hoteldeleon.jpg";
-import drecepcion from "../../assets/hoteles/deLeon/hoteldeleon-recepcion.jpg";
-import dhabitacion from "../../assets/hoteles/deLeon/hoteldeleon-habitacion.jpg";
+import dfachada from '../../assets/hoteles/deLeon/Hoteldeleon.jpg';
+import drecepcion from '../../assets/hoteles/deLeon/hoteldeleon-recepcion.jpg';
+import dhabitacion from '../../assets/hoteles/deLeon/hoteldeleon-habitacion.jpg';
 
-import efachada from "../../assets/hoteles/Estoraques/hotelestoraque.png";
-import erecepcion from "../../assets/hoteles/Estoraques/hotelestoraque-recepcion.png";
-import ehabitacion from "../../assets/hoteles/Estoraques/hotelestoraque-habitacion.png";
-
-
+import efachada from '../../assets/hoteles/Estoraques/hotelestoraque.png';
+import erecepcion from '../../assets/hoteles/Estoraques/hotelestoraque-recepcion.png';
+import ehabitacion from '../../assets/hoteles/Estoraques/hotelestoraque-habitacion.png';
 
 export default function Tab3() {
   const accordionGroup = useRef<null | HTMLIonAccordionGroupElement>(null);
@@ -30,52 +30,22 @@ export default function Tab3() {
       return;
     }
 
-    accordionGroup.current.value = ["first", "third"];
+    accordionGroup.current.value = ['first', 'third'];
   }, []);
 
   return (
-    <IonAccordionGroup ref={accordionGroup} multiple={true}>
-      <IonAccordion value="first">
-        <IonItem slot="header" color="light">
-          <IonLabel>Morrocoy</IonLabel>
-        </IonItem>
-        <div className="ion-padding" slot="content">
-          <HotelView
-            title="HOTEL MORROCOY"
-            img1={mfachada}
-            img2={mrecepcion}
-            img3={mhabitacion}
-          />
+    <IonPage>
+      <IonContent fullscreen>
+        <div style={{ width: '100%', height: '100%' }}>
+          <iframe
+            src='https://storage.googleapis.com/maps-solutions-9czm781ipm/neighborhood-discovery/7hyt/neighborhood-discovery.html'
+            width='100%'
+            height='100%'
+            style={{ border: 0 }}
+            loading='lazy'
+          ></iframe>
         </div>
-      </IonAccordion>
-
-      <IonAccordion value="second">
-        <IonItem slot="header" color="light">
-          <IonLabel>De Leon</IonLabel>
-        </IonItem>
-        <div className="ion-padding" slot="content">
-          <HotelView
-            title="HOTEL D LEON"
-            img1={dfachada}
-            img2={drecepcion}
-            img3={dhabitacion}
-          />
-        </div>
-      </IonAccordion>
-
-      <IonAccordion value="third">
-        <IonItem slot="header" color="light">
-          <IonLabel>Los Estoraques</IonLabel>
-        </IonItem>
-        <div className="ion-padding" slot="content">
-          <HotelView
-            title="HOTEL ESTORAQUES"
-            img1={efachada}
-            img2={erecepcion}
-            img3={ehabitacion}
-          />
-        </div>
-      </IonAccordion>
-    </IonAccordionGroup>
+      </IonContent>
+    </IonPage>
   );
 }
